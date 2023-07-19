4894cda66593cd2b6c1e00f984d3c60c6b301994
Vitor Gabriel <vitorgabrielsbo1460@gmail.com>
2023-07-19 00:25:52 -0300

    b

--- app/page.jsx
+++ app/page.jsx
@@ -1,36 +1,8 @@
-"use client"
-
-// Imports Components
-import Feed from '@components/Feed'
-import Sidebar from '@components/Sidebar/Sidebar'
-import { useSession } from 'next-auth/react'
-import { useEffect, useState } from 'react'
-import Loader from '@components/Loader'
-
 export default function Home() {
 
-  const { data: session } = useSession()
-  const [data, setData] = useState([])
-
-  const getUser = async () => {
-    const response = await fetch(`/api/user/${session?.user.id}`)
-    const answer = await response.json()
-
-    setData(answer)
-  }
-
-  useEffect(() => {
-    if (session) {
-      getUser()
-    }
-  }, [session])
-
-  return session ? (
+  return (
     <div className='home-container'>
-      <Feed data={data} />
-      <Sidebar data={data} />
+      hello world
     </div>
-  ) : (
-    <Loader />
-  )
+  ) 
 }
