01bcbabbe8fd16ee07471d766e47bffac24d0478
Vitor Gabriel <vitorgabrielsbo1460@gmail.com>
2023-10-26 19:25:26 -0300

    bugs fixed

--- app/api/posts/like/route.js
+++ app/api/posts/like/route.js
@@ -11,24 +11,26 @@ export const POST = async (request) => {
     if (userId) {
       const postagem = await Postagem.findOne({_id: postId})
 
-      if (!postagem.idsCurtidas.includes(userId)) {
-        postagem.idsCurtidas += ` ${userId}`
-        postagem.curtidas += 1
-
-      } else {
-
-        const curtidas_separadas = postagem.idsCurtidas.split(" ")
-        const indice = curtidas_separadas.indexOf(userId)
-        
-        curtidas_separadas.splice(indice, 1)
-        postagem.idsCurtidas = curtidas_separadas.join(" ")
-
-        postagem.curtidas -= 1
+      if (postagem) {
+        if (!postagem.idsCurtidas.includes(userId)) {
+          postagem.idsCurtidas += ` ${userId}`
+          postagem.curtidas += 1
+  
+        } else {
+  
+          const curtidas_separadas = postagem.idsCurtidas.split(" ")
+          const indice = curtidas_separadas.indexOf(userId)
+          
+          curtidas_separadas.splice(indice, 1)
+          postagem.idsCurtidas = curtidas_separadas.join(" ")
+  
+          postagem.curtidas -= 1
+        }
+  
+        await postagem.save()
+  
+        return new Response("Postagem foi curtida", {status: 200})
       }
-
-      await postagem.save()
-
-      return new Response("Postagem foi curtida", {status: 200})
     }
 
   } catch (error) {
