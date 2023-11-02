962fd94609eea4a5045e3adc54804a97857af7da
Vitor Gabriel <vitorgabrielsbo1460@gmail.com>
2023-11-02 00:52:15 -0300

    tests

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: '/Tcc',
     experimental: {
         appDir: true,
         serverComponentsExternalPackages: ["mongoose"],
