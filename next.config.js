4fe7108d277a8c37ef7681f5825c1c9570a5ab23
Vitor Gabriel <vitorgabrielsbo1460@gmail.com>
2023-11-02 01:01:04 -0300

    bugs fixed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/Tcc',
     experimental: {
         appDir: true,
         serverComponentsExternalPackages: ["mongoose"],
